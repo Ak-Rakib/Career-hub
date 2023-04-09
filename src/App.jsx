59c211aef294f3aacd24a9a703ad092d1445e236
Ak-Rakib <rkrakibsaimon@gmail.com>
2023-04-10 02:45:56 +0600

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Footer from './Components/Footer/Footer'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default App
