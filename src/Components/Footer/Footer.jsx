b96fb5dab4ff628749069fc22eeb40dbb43a8558
Ak-Rakib <rkrakibsaimon@gmail.com>
2023-04-10 16:51:15 +0600

    footer done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import React from "react";
+import logo from '../../assets/Logo/main-logo.png'
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
-    return (
-        <div>
-             <h1>I'm Footer</h1>
+  return (
+    <div>
+      <footer className="footer p-10 bg-base-200 text-base-content">
+        <div className="flex flex-col items-center">
+          <div>
+          <img className="w-20" src={logo} alt="" />
+          </div>
+          <p>
+            ACME Industries Ltd.
+            <br />
+            Providing reliable tech since 1992
+          </p>
+          <div className="flex justify-between">
+            <FaFacebook></FaFacebook>
+            <FaTwitter></FaTwitter>
+            <FaInstagram></FaInstagram>
+          </div>
         </div>
-    );
+        <div>
+          <span className="footer-title">Company</span>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </div>
+        <div>
+          <span className="footer-title">Product</span>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </div>
+        <div>
+          <span className="footer-title">Support</span>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </div>
+        <div>
+          <span className="footer-title">Contact</span>
+          <a className="link link-hover">524 Broadway , NYC</a>
+          <a className="link link-hover">+1 777 - 978 - 5570</a>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
