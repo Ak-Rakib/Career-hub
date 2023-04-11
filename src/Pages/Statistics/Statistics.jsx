6d6b979fa8c8a2e1688e61db568cde357c01ee2b
Ak-Rakib <rkrakibsaimon@gmail.com>
2023-04-11 23:07:35 +0600

    charts correction

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import vector from "../../assets/All Images/Vector.png";
 import vector1 from "../../assets/All Images/Vector-1.png";
 import { useLoaderData } from "react-router-dom";
-import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
-
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 const Statistics = () => {
   const data = useLoaderData();
@@ -26,31 +33,41 @@ const Statistics = () => {
       </div>
       <div className="mb-10">
         <ResponsiveContainer width="95%" height={400}>
-          <LineChart
-            className="chart"
-            width={500}
-            height={300}
+          <AreaChart
+            width={730}
+            height={250}
             data={data}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
+            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
           >
-            <CartesianGrid strokeDasharray="3 3" />
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+              </linearGradient>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+              </linearGradient>
+            </defs>
             <XAxis dataKey="name" />
             <YAxis />
+            <CartesianGrid strokeDasharray="3 3" />
             <Tooltip />
-            <Legend />
-            <Line
+            <Area
+              type="monotone"
+              dataKey="uv"
+              stroke="#8884d8"
+              fillOpacity={1}
+              fill="url(#colorUv)"
+            />
+            <Area
               type="monotone"
               dataKey="pv"
-              stroke="#8884d8"
-              activeDot={{ r: 8 }}
+              stroke="#82ca9d"
+              fillOpacity={1}
+              fill="url(#colorPv)"
             />
-            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-          </LineChart>
+          </AreaChart>
         </ResponsiveContainer>
       </div>
     </div>
