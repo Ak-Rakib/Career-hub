5f9b6dc91acb715d43f347b22a7cbbf6f5f33f97
Ak-Rakib <rkrakibsaimon@gmail.com>
2023-11-30 22:41:28 +0600

    set modal but some issues already running

--- src/Pages/AppliedJob/AppliedJobData/AppliedJobData.jsx
+++ src/Pages/AppliedJob/AppliedJobData/AppliedJobData.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const AppliedJobData = ({ data }) => {
@@ -14,15 +14,39 @@ const AppliedJobData = ({ data }) => {
     venue,
     id,
   } = data;
+
+  // --------------------------- Modal --------------------------
+  const [modalIsOpen, setIsIOpen] = useState(false);
+  const MyModal = () => {
+    useEffect(() => {
+      document.body.style.overflowY = "hidden"
+      return () => {
+        document.body.style.overflowY = "scroll"
+      }
+    },[]);
+    return (
+      <>
+        <div className="modal-wrapper" onClick={() => setIsIOpen(false)}></div>
+        <div className="modal-container">
+          <h2 className="modal-header">Successfully Confirm!!</h2>
+          <p className="modal-p">Stay Tuned, We will inform you via this website and Email</p>
+          <button
+            onClick={() => setIsIOpen(false)}
+            className="btn border-0 bg-gradient-to-r from-blue-500 to-purple-500"
+          >
+            Close
+          </button>
+        </div>
+      </>
+    );
+  };
+  // --------------------------------------------------------------
+
   return (
     <div>
       <div className="card card-side bg-base-200 shadow-xl md:m-10">
         <figure>
-          <img
-            className="ps-3"
-            src={icon}
-            alt="Company logo"
-          />
+          <img className="ps-3" src={icon} alt="Company logo" />
         </figure>
         <div className="card-body">
           <h2 className="card-title">{position}</h2>
@@ -38,13 +62,23 @@ const AppliedJobData = ({ data }) => {
             <span className="text-gray-400 font-normal">{salary}</span>
           </div>
           <div className="card-actions flex justify-end items-center">
-            <button className="btn border-0 bg-gradient-to-r from-blue-500 to-purple-500">confirm</button>
-            <button className="btn border-0 bg-gradient-to-r from-blue-500 to-purple-500"><Link to="/">back home</Link></button>
+            <button
+              onClick={() => setIsIOpen(true)}
+              className="btn border-0 bg-gradient-to-r from-blue-500 to-purple-500"
+            >
+              confirm
+            </button>
+
+            <button className="btn border-0 bg-gradient-to-r from-blue-500 to-purple-500">
+              <Link to="/">Remove</Link>
+            </button>
           </div>
         </div>
       </div>
+      <div>{modalIsOpen && <MyModal />}</div>
     </div>
   );
 };
 
+// ReactDOM.render(setAppElement);
 export default AppliedJobData;
